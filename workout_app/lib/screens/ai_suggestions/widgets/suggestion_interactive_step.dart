cf18f8f752b8731292872aeb249c894287acc9e9
Hoang Nhan <tranhoangnhan10e@gmail.com>
2026-04-11 01:22:34 +0700

    clean shi

---
+++ workout_app/lib/screens/ai_suggestions/widgets/suggestion_interactive_step.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:shared/shared.dart';
+import '../../../widgets/loading_animation.dart';
+import '../ai_suggestions_screen.dart';
+import 'health_edit_form.dart';
+import 'health_validation_card.dart';
+
+class SuggestionInteractiveStep extends StatelessWidget {
+  final AISuggestionStep currentStep;
+  final HealthUpdateParams healthState;
+  final VoidCallback onConfirm;
+  final VoidCallback onEdit;
+  final Function(HealthUpdateParams) onSaveEdit;
+  final VoidCallback onCancelEdit;
+
+  const SuggestionInteractiveStep({
+    super.key,
+    required this.currentStep,
+    required this.healthState,
+    required this.onConfirm,
+    required this.onEdit,
+    required this.onSaveEdit,
+    required this.onCancelEdit,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    switch (currentStep) {
+      case AISuggestionStep.validating:
+        return HealthValidationCard(
+          healthData: healthState,
+          onConfirm: onConfirm,
+          onEdit: onEdit,
+        );
+      case AISuggestionStep.editing:
+        return HealthEditForm(
+          initialState: healthState,
+          onSave: onSaveEdit,
+          onCancel: onCancelEdit,
+        );
+      case AISuggestionStep.askingRequirement:
+        return const SizedBox.shrink();
+      case AISuggestionStep.generating:
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 20),
+          child: AppLoading(
+            message: 'AI đang phân tích dữ liệu của bạn...',
+          ),
+        );
+      case AISuggestionStep.results:
+        return const SizedBox.shrink();
+    }
+  }
+}
